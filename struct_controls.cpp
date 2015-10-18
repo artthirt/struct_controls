cfdc1c9544da4c7ba4adee2890f09673cbe6ce71
catalips <artthirt@gmail.com>
2015-10-18 19:32:33 +0300

    add structures for work with servo

---
+++ struct_controls.cpp
@@ -0,0 +1,227 @@
+#include "struct_controls.h"
+
+using namespace sc;
+
+StructServo::StructServo()
+{
+	freq_meandr = 0;
+	angle = 0;
+	speed_of_change = 0;
+	timework_ms = 0;
+	flag_start = false;
+	pin = 0;
+}
+
+void StructServo::write_to(QDataStream &stream)
+{
+	stream << freq_meandr;
+	stream << angle;
+	stream << speed_of_change;
+	stream << timework_ms;
+	stream << flag_start;
+	stream << pin;
+}
+
+void StructServo::read_from(QDataStream &stream)
+{
+	stream >> freq_meandr;
+	stream >> angle;
+	stream >> speed_of_change;
+	stream >> timework_ms;
+	stream >> flag_start;
+	stream >> pin;
+}
+
+bool StructServo::trigger_start(const StructServo &last) const
+{
+	return flag_start && !last.flag_start;
+}
+
+////////////////////////////////////////////////////
+
+StructControls::StructControls()
+{
+	power_on = false;
+	throttle = 0;
+	tangaj = 0;
+	bank = 0;
+	yaw = 0;
+}
+
+void StructControls::write_to(QDataStream &stream)
+{
+	stream.setByteOrder(QDataStream::BigEndian);
+	stream.setFloatingPointPrecision(QDataStream::SinglePrecision);
+	stream.setVersion(QDataStream::Qt_4_8);
+
+	stream << power_on;
+	stream << throttle;
+	stream << tangaj;
+	stream << bank;
+	stream << yaw;
+	servo_ctrl.write_to(stream);
+}
+
+void StructControls::read_from(QDataStream &stream)
+{
+	stream.setByteOrder(QDataStream::BigEndian);
+	stream.setFloatingPointPrecision(QDataStream::SinglePrecision);
+	stream.setVersion(QDataStream::Qt_4_8);
+
+	stream >> power_on;
+	stream >> throttle;
+	stream >> tangaj;
+	stream >> bank;
+	stream >> yaw;
+	servo_ctrl.read_from(stream);
+}
+
+////////////////////////////////////////////////////
+
+StructGyroscope::StructGyroscope()
+{
+	fs_sel = 0;
+	afs_sel = 0;
+	freq = default_freq;
+	tick = 0;
+	temp = 0;
+	FOREACH(i, raw_count, raw[i] = 0);
+}
+
+StructGyroscope::StructGyroscope(const StructGyroscope& st)
+{
+	gyro = st.gyro;
+	accel = st.accel;
+	afs_sel = st.afs_sel;
+	fs_sel = st.fs_sel;
+	freq = st.freq;
+	tick = st.tick;
+	std::copy(st.raw, st.raw + raw_count, raw);
+	temp = st.temp;
+}
+
+void StructGyroscope::write_to(QDataStream& stream)
+{
+	stream << temp;
+	FOREACH(i, vector3_::Vector3i::count, stream << gyro[i]);
+	FOREACH(i, vector3_::Vector3i::count, stream << accel[i]);
+	stream << afs_sel;
+	stream << fs_sel;
+	stream << freq;
+	stream << tick;
+	stream.writeRawData(reinterpret_cast< char* >(raw), raw_count);
+}
+
+/**
+ * @brief read_from
+ * deserialize byte array
+ * @param stream
+ */
+void StructGyroscope::read_from(QDataStream& stream)
+{
+	stream >> temp;
+	FOREACH(i, vector3_::Vector3i::count, stream >> gyro[i]);
+	FOREACH(i, vector3_::Vector3i::count, stream >> accel[i]);
+	stream >> afs_sel;
+	stream >> fs_sel;
+	stream >> freq;
+	stream >> tick;
+	stream.readRawData(reinterpret_cast< char* >(raw), raw_count);
+}
+
+vector3_::Vector3d StructGyroscope::angular_speed(const vector3_::Vector3d& offset)
+{
+	float factor = 1.0;
+	vector3_::Vector3d g = gyro;
+	g -= offset;
+	switch (fs_sel) {
+		case 0:
+		default:
+			factor = 250.0f / 32768.0f;
+			break;
+		case 1:
+			factor = 500.0f / 32768.0f;
+			break;
+		case 2:
+			factor = 1000.0f / 32768.0f;
+			break;
+		case 3:
+			factor = 2000.0f / 32768.0f;
+			break;
+	}
+	vector3_::Vector3d res = vector3_::Vector3d(g);
+	if(!freq)
+		freq = default_freq;
+
+	res *= factor;
+	return res;
+}
+
+////////////////////////////////////////////////
+////////////////////////////////////////////////
+
+/**
+ * @brief StructTelemetry
+ */
+StructTelemetry::StructTelemetry()
+{
+	FOREACH(i, cnt_engines, power[i] = 0;);
+	power_on = false;
+	height = 0;
+	course = tangaj = bank = 0;
+}
+
+/**
+ * @brief StructTelemetry
+ * @param st
+ */
+StructTelemetry::StructTelemetry(const StructTelemetry& st)
+{
+	power_on = st.power_on;
+	FOREACH(i, cnt_engines, power[i] = st.power[i];);
+	tangaj = st.tangaj;
+	bank = st.bank;
+	course = st.course;
+	height = st.height;
+	gyroscope = st.gyroscope;
+}
+
+/**
+ * @brief write_to
+ * serialize to byte array
+ * @param stream
+ */
+void StructTelemetry::write_to(QDataStream& stream)
+{
+	stream.setByteOrder(QDataStream::BigEndian);
+	stream.setFloatingPointPrecision(QDataStream::SinglePrecision);
+	stream.setVersion(QDataStream::Qt_4_8);
+
+	stream << power_on;
+	FOREACH(i, cnt_engines, stream << power[i]);
+	stream << tangaj;
+	stream << bank;
+	stream << course;
+	stream << height;
+	gyroscope.write_to(stream);
+}
+
+/**
+ * @brief read_from
+ * deserialize byte array
+ * @param stream
+ */
+void StructTelemetry::read_from(QDataStream& stream)
+{
+	stream.setByteOrder(QDataStream::BigEndian);
+	stream.setFloatingPointPrecision(QDataStream::SinglePrecision);
+	stream.setVersion(QDataStream::Qt_4_8);
+
+	stream >> power_on;
+	FOREACH(i, cnt_engines, stream >> power[i]);
+	stream >> tangaj;
+	stream >> bank;
+	stream >> course;
+	stream >> height;
+	gyroscope.read_from(stream);
+}
--- struct_controls.h
+++ struct_controls.h
@@ -11,18 +11,73 @@
 
 namespace sc{
 
+struct StructServo{
+	StructServo();
+
+	void write_to(QDataStream& stream);
+	void read_from(QDataStream& stream);
+	bool trigger_start(const StructServo& last) const;
+
+	float freq_meandr;
+	float angle;
+	float speed_of_change;
+	float timework_ms;
+
+	bool flag_start;
+	int pin;
+};
+
 struct StructControls
 {
+	StructControls();
+
+	void write_to(QDataStream& stream);
+	void read_from(QDataStream& stream);
+
 	bool power_on;
 	float throttle;
 	float tangaj;
 	float bank;
-	float rotation;
+	float yaw;
+	StructServo servo_ctrl;
 };
 
 const int cnt_engines = 4;
 const int raw_count = 46;
 const float default_freq = 100;
+
+struct StructGyroscope{
+
+	StructGyroscope();
+	StructGyroscope(const StructGyroscope& st);
+	void write_to(QDataStream& stream);
+	/**
+	 * @brief read_from
+	 * deserialize byte array
+	 * @param stream
+	 */
+	void read_from(QDataStream& stream);
+	/**
+	 * @brief angular_speed
+	 * get angular speed from gyroscope mpu6050
+	 * uses fs_sel for get factor to get the real angles
+	 * @param offset
+	 * @return
+	 */
+	vector3_::Vector3d angular_speed(const vector3_::Vector3d& offset = vector3_::Vector3d());
+
+	vector3_::Vector3i gyro;
+	vector3_::Vector3i accel;
+
+	unsigned char afs_sel;				/// value of accelerometer mode
+	unsigned char fs_sel;				/// value of gyroscope mode
+	float temp;
+	float freq;							/// It is used to define part of the data
+										/// from the sensors to a single point in time
+	long long tick;
+	unsigned char raw[raw_count];		/// raw data from 0x0d to 0x3a address from mpu6050
+
+};
 
 /**
  * @brief The StructTelemetry struct
@@ -33,122 +88,24 @@ struct StructTelemetry
 	/**
 	 * @brief StructTelemetry
 	 */
-	StructTelemetry(){
-		FOREACH(i, cnt_engines, power[i] = 0;);
-		power_on = false;
-		height = 0;
-		temp = 0;
-		course = tangaj = bank = 0;
-		fs_sel = 0;
-		afs_sel = 0;
-		freq = default_freq;
-		tick = 0;
-		FOREACH(i, raw_count, raw[i] = 0);
-	}
+	StructTelemetry();
 	/**
 	 * @brief StructTelemetry
 	 * @param st
 	 */
-	StructTelemetry(const StructTelemetry& st){
-		power_on = st.power_on;
-		FOREACH(i, cnt_engines, power[i] = st.power[i];);
-		tangaj = st.tangaj;
-		bank = st.bank;
-		course = st.course;
-		gyro = st.gyro;
-		accel = st.accel;
-		height = st.height;
-		temp = st.temp;
-		afs_sel = st.afs_sel;
-		fs_sel = st.fs_sel;
-		freq = st.freq;
-		tick = st.tick;
-		std::copy(st.raw, st.raw + raw_count, raw);
-	}
+	StructTelemetry(const StructTelemetry& st);
 	/**
 	 * @brief write_to
 	 * serialize to byte array
 	 * @param stream
 	 */
-	void write_to(QDataStream& stream){
-		stream.setByteOrder(QDataStream::BigEndian);
-		stream.setFloatingPointPrecision(QDataStream::SinglePrecision);
-		stream.setVersion(QDataStream::Qt_4_8);
-
-		stream << power_on;
-		FOREACH(i, cnt_engines, stream << power[i]);
-		stream << tangaj;
-		stream << bank;
-		stream << course;
-		stream << temp;
-		stream << height;
-		FOREACH(i, vector3_::Vector3i::count, stream << gyro[i]);
-		FOREACH(i, vector3_::Vector3i::count, stream << accel[i]);
-		stream << afs_sel;
-		stream << fs_sel;
-		stream << freq;
-		stream << tick;
-		stream.writeRawData(reinterpret_cast< char* >(raw), raw_count);
-	}
+	void write_to(QDataStream& stream);
 	/**
 	 * @brief read_from
 	 * deserialize byte array
 	 * @param stream
 	 */
-	void read_from(QDataStream& stream){
-		stream.setByteOrder(QDataStream::BigEndian);
-		stream.setFloatingPointPrecision(QDataStream::SinglePrecision);
-		stream.setVersion(QDataStream::Qt_4_8);
-
-		stream >> power_on;
-		FOREACH(i, cnt_engines, stream >> power[i]);
-		stream >> tangaj;
-		stream >> bank;
-		stream >> course;
-		stream >> temp;
-		stream >> height;
-		FOREACH(i, vector3_::Vector3i::count, stream >> gyro[i]);
-		FOREACH(i, vector3_::Vector3i::count, stream >> accel[i]);
-		stream >> afs_sel;
-		stream >> fs_sel;
-		stream >> freq;
-		stream >> tick;
-		stream.readRawData(reinterpret_cast< char* >(raw), raw_count);
-	}
-
-	/**
-	 * @brief angular_speed
-	 * get angular speed from gyroscope mpu6050
-	 * uses fs_sel for get factor to get the real angles
-	 * @param offset
-	 * @return
-	 */
-	vector3_::Vector3d angular_speed(const vector3_::Vector3d& offset = vector3_::Vector3d()){
-		float factor = 1.0;
-		vector3_::Vector3d g = gyro;
-		g -= offset;
-		switch (fs_sel) {
-			case 0:
-			default:
-				factor = 250.0f / 32768.0f;
-				break;
-			case 1:
-				factor = 500.0f / 32768.0f;
-				break;
-			case 2:
-				factor = 1000.0f / 32768.0f;
-				break;
-			case 3:
-				factor = 2000.0f / 32768.0f;
-				break;
-		}
-		vector3_::Vector3d res = vector3_::Vector3d(g);
-		if(!freq)
-			freq = default_freq;
-
-		res *= factor;
-		return res;
-	}
+	void read_from(QDataStream& stream);
 
 	bool power_on;
 
@@ -157,19 +114,10 @@ struct StructTelemetry
 	float tangaj;
 	float bank;
 	float course;
-	float temp;
 	float height;
 
-	vector3_::Vector3i gyro;
+	StructGyroscope gyroscope;
 
-	vector3_::Vector3i accel;
-
-	unsigned char afs_sel;				/// value of accelerometer mode
-	unsigned char fs_sel;				/// value of gyroscope mode
-	float freq;							/// It is used to define part of the data
-										/// from the sensors to a single point in time
-	long long tick;
-	unsigned char raw[raw_count];		/// raw data from 0x0d to 0x3a address from mpu6050
 };
 
 }
